e6a656b04dafb6ce1a46bddcfe4799ea91d6b9e7
Michael Fortunato <Michaelfortunato@uchicago.edu>
2020-07-30 01:56:43 -0400

    working on button

--- src/components/Nav/Navbutton.js
+++ src/components/Nav/Navbutton.js
@@ -3,84 +3,166 @@ import styled from 'styled-components'
 import { CSSTransition } from 'react-transition-group'
 
 const StyledNavbutton = styled.div`
-/* Icon 4 */
     position: absolute;
     top: 5%;
     right: 5%;
-    color :#black; 
-    font-size: 45px;
+    color :#F5F5F5; 
+    font-size: 65px;
     z-index: 1;
-
-    .nav-icon1, {
-        width: 60px;
-        height: 45px;
-        position: relative;
-        margin: 50px auto;
-        transform: rotate(0deg);
-        transition: .5s ease-in-out;
-        cursor: pointer;
-      }
-      
-      .patty2-exit{
-        display: block;
-        position: absolute;
-        height: 9px;
-        width: 100%;
-        background: #d3531a;
-        border-radius: 9px;
-        opacity: 1;
-        left: 0;
-
-        transform: rotate(0deg);
-        transition: .25s ease-in-out;
-      }
-      
-      &.patty1-open-appear &.patty1-open-enter  {
-        top: 0px;
-      }
-      
-      &.patty2-open-appear &.patty2-open-enter  {
-        top: 18px;
-      }
-      
-      &.patty3-open-appear &.patty3-open-enter  {
-        top: 36px;
-      }
-      
-      &.patty1-open-appear-active &.patty1-open-enter-active  {
-        top: 18px;
-        transform: rotate(135deg);
-      }
-      
-      &.patty2-open-appear-active &.patty2-open-enter-active  {
-        opacity: 0;
-        left: -60px;
-      }
-      
-      &.patty1-open-appear-done &.patty1-open-enter-done{
-        top: 18px;
-        transform: rotate(-135deg);
-      }
+    
+    width: 60px;
+    height: 45px;
+    margin: 50px auto;
+    cursor: pointer;
+  
+  #nav-icon1 {
+    display: block;
+    position: absolute;
+    height: 9px;
+    width: 100%;
+    background: #d3531a;
+    border-radius: 9px;
+    opacity: 1;
+    left: 0;
+    transform: rotate(0deg);
+    transition: .25s ease-in-out;
+  }
+  
+  #nav-icon1 span:nth-child(1) {
+    top: 0px;
+  }
+  
+  #nav-icon1 span:nth-child(2) {
+    top: 18px;
+  }
+  
+  #nav-icon1 span:nth-child(3) {
+    top: 36px;
+  }
+  
+  #nav-icon1.open span:nth-child(1) {
+    top: 18px;
+    transform: rotate(135deg);
+  }
+  
+  #nav-icon1.open span:nth-child(2) {
+    opacity: 0;
+    left: -60px;
+  }
+  
+  #nav-icon1.open span:nth-child(3) {
+    top: 18px;
+    transform: rotate(-135deg);
+  }
 `;
 
+
+const StyledPatty1 = styled.span`
+    display: block;
+    position: absolute;
+
+    height: 5px;
+    width: 100%;
+
+    background: black;
+
+    top: 0px;
+    left: 0;
+
+    border-radius: 9px;
+    opacity: 1;
+
+    &.patty-appear, &.patty-enter  {}
+    &.patty-appear-active, &.patty-enter-active {
+        top: 18px;
+        transform: rotate(135deg);
+        transition: all 250ms ease-in-out;
+    }
+
+    &.patty-appear-done, &.patty-enter-done {
+        top: 18px;
+        transform: rotate(135deg);
+    }
+
+    &.patty-exit {}
+    &.patty-exit-active {
+        top: 0px;
+        transform: rotate(0deg);
+        transition: all .25s ease-in-out;
+    }
+    &.patty-exit-done {
+        top: 0px;
+        transform: rotate(0deg);
+    }
+
+}
+
+
+`;
+const StyledPatty2 = styled.span`
+`;
+
+const StyledPatty3 = styled.span` 
+    display: block;
+    position: absolute;
+
+    height: 5px;
+    width: 100%;
+
+    background: black;
+
+    top: 36px;
+    left: 0;
+
+    border-radius: 9px;
+    opacity: 1;
+
+    &.patty-appear, &.patty-enter  {}
+    &.patty-appear-active, &.patty-enter-active {
+        top: 18px;
+        transform: rotate(-135deg);
+        transition: all 250ms ease-in-out;
+    }
+
+    &.patty-appear-done, &.patty-enter-done {
+        top: 18px;
+        transform: rotate(-135deg);
+    }
+
+    &.patty-exit {}
+    &.patty-exit-active {
+        top: 36px;
+        transform: rotate(0deg);
+        transition: all 250ms ease-in-out;
+    }
+    &.patty-exit-done {
+        top: 36px;
+        transform: rotate(0deg);
+    }
+
+}
+`;
 
 const Navbutton = (props) =>  {
     return(
  
-    <CSSTransition
+
+
+   /* <CSSTransition
         classNames = 'open'
         timeout = {}
 
         
-    >
-    <StyledNavbutton>
-    <div className="nav-icon1">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+    > */
+    <StyledNavbutton onClick = {() => props.setIsVisible(! props.isVisible)}>
+        <CSSTransition in = {!props.isVisible} classNames = 'patty' timeout = {250}>
+            <StyledPatty1/>
+        </CSSTransition>
+        <CSSTransition in = {!props.isVisible} classNames = 'patty' timeout = {250}>
+            <StyledPatty3 />
+        </CSSTransition>
     </StyledNavbutton>
-    </CSSTransition>
+    //</CSSTransition>
  
     );
 }
